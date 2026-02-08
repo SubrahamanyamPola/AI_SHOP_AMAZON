9f2d4a063adf6c4521398cdf670a5d7f9e3379b5
SubrahamanyamPola <164750699+SubrahamanyamPola@users.noreply.github.com>
2026-02-08 12:11:26 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, sys
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_shop.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_shop.settings.prod')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 if __name__ == '__main__':
